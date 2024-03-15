4a56e0eeb9c3240a7c9e475871bee85507e94cb0
echo <play3step@naver.com>
2024-03-16 00:26:02 +0900

    feat: 선택정보 로그 추가

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -25,22 +25,24 @@ function App() {
   };
 
   return (
-    <div id="game-container">
-      <ol id="players" className="highlight-player">
-        <Player
-          initialName="Player 1"
-          symbol="X"
-          isActive={activePlayer === "X"}
-        />
-        <Player
-          initialName="Player 2"
-          symbol="O"
-          isActive={activePlayer === "O"}
-        />
-      </ol>
-      <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
-      <Log />
-    </div>
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
+        </ol>
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+      </div>
+      <Log turns={gameTurns} />
+    </main>
   );
 }
 
