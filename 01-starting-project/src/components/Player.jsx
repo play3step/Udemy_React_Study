db3dcf6ab99edd224a10f100e06340366ef70ed3
echo <play3step@naver.com>
2024-03-18 22:52:18 +0900

    feat: Refs를 활용한 input 값 변경

---
+++ 01-starting-project/src/components/Player.jsx
@@ -0,0 +1,19 @@
+import { useRef, useState } from "react";
+
+export default function Player() {
+  const playerName = useRef();
+  const [enterPlayerName, setEnterPlayerName] = useState("");
+
+  const handleClick = () => {
+    setEnterPlayerName(playerName.current.value);
+  };
+  return (
+    <section id="player">
+      <h2>Welcome {enterPlayerName ?? " unknown entity"}</h2>
+      <p>
+        <input ref={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
